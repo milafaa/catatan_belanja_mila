1f0540a171d2d1864d8eefedbcb203cba96d67f6
milaaaa4 <milakuliah5758@gmail.com>
2026-03-08 12:03:44 +0700

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/catatan-belanja-mila/',
+  base: '/catatan_belanja_mila/',
   plugins: [react()],
 })
